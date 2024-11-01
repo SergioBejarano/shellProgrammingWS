74d4dc18987e4da83631c6a84ae0d238e0f92393
SergioBejarano <157920809+SergioBejarano@users.noreply.github.com>
2024-11-01 11:51:46 -0500

    Update permisos.ps1

--- permisos.ps1
+++ permisos.ps1
@@ -1,10 +1,9 @@
 
 function punto5 {
     Clear-Host
-    param (
-    [string]$directorio,       # Directorio donde buscar archivos
-    [string]$permiso    # Permiso que se desea buscar (ej. "ReadData", "Write", "FullControl")
-    )
+    $directorio = Read-Host "Introduce el directorio"
+    $permiso = Read-Host "Introduce el permiso"
+
     if (-Not (Test-Path -Path $directorio -PathType Container)) {
         Write-Host "Directorio inválido"
         exit 1
@@ -13,5 +12,6 @@ function punto5 {
     Get-ChildItem -Path $directorio -File | Where-Object {
         (Get-Acl $_.FullName).AccessToString -match $permiso
     } | Format-Table FullName
-    }
+}
+
 punto5 -directorio $args[0] -permiso $args[1]
